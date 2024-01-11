4b335be707aa2b840037df3f5beea83832562626
shakhzod9575 <shahzodelturayevepam@gmail.com>
2024-01-11 18:37:43 +0500

    bro

---
+++ src/main/java/com/example/testverceldeployment/TestVercelDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.testverceldeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVercelDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestVercelDeploymentApplication.class, args);
+    }
+
+}
